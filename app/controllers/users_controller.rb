225f648880801870d2eb62f87c7a6078f6537ba6
Chris Everett <mistermaniac309@gmail.com>
2015-03-12 00:28:21 +0000

    Adding views for new and show, displaying users at /Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
   end
   
   def create
-    @user = User.new(params[:user])
+  @user = User.new(user_params)
 
   @user.save
   redirect_to @user
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :employee_Id, :employee_type)
   end
 end
