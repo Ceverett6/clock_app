744c7e0d932e04365b6abb9673455621508cdebd
Chris Everett <mistermaniac309@gmail.com>
2015-03-12 19:52:19 +0000

    Add validation for Users form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true,
+                    length: { minimum: 1 }
+                    
+  validates :last_name, presence: true,
+                    length: { minimum: 1 }
+                    
+  validates :employee_Id, presence: true,
+                    length: { minimum: 1 },
+                    uniqueness: true
+                    
+  validates :employee_type, presence: true,
+                    length: { minimum: 1 }                  
 end
